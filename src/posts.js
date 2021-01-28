40393ce203f9ba7c08ef22e90a693cd12570b0a2
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-01-28 17:18:22 +1300

    Customize PostEdit title

--- src/posts.js
+++ src/posts.js
@@ -26,8 +26,12 @@ export const PostList = (props) => (
   </List>
 );
 
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
 export const PostEdit = (props) => (
-  <Edit {...props}>
+  <Edit title={<PostTitle />} {...props}>
     <SimpleForm>
       <TextInput disabled source="id" />
       <ReferenceInput source="userId" reference="users">
