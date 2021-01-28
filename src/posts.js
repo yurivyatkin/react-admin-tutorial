af3ba32a3edf0910b179e830eba77f5ef5bd1b39
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-01-29 00:00:48 +1300

    Add Search And Filters

--- src/posts.js
+++ src/posts.js
@@ -1,20 +1,30 @@
 import * as React from 'react';
 import {
-  List,
-  Datagrid,
-  TextField,
-  ReferenceField,
-  EditButton,
-  Edit,
   Create,
-  SimpleForm,
+  Datagrid,
+  Edit,
+  EditButton,
+  Filter,
+  List,
+  ReferenceField,
   ReferenceInput,
   SelectInput,
+  SimpleForm,
+  TextField,
   TextInput,
 } from 'react-admin';
 
+const PostFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
+
 export const PostList = (props) => (
-  <List {...props}>
+  <List filters={<PostFilter />} {...props}>
     <Datagrid rowClick="edit">
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
