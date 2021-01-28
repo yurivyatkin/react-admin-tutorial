7aae7caad84ec4a212b41ee2c77534133157524b
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-01-29 00:14:27 +1300

    Customize Home Page

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import UserIcon from '@material-ui/icons/Group';
 
 import jsonServerProvider from 'ra-data-json-server';
 
+import Dashboard from './Dashboard';
 import { UserList } from './users';
 import { PostList, PostEdit, PostCreate } from './posts';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource
       name="posts"
       list={PostList}
