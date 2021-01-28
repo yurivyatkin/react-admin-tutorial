287bc453cdfbebeaecec4cef95de15102c88e54e
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-01-28 16:27:13 +1300

    Reorder and cleanup columns

--- src/posts.js
+++ src/posts.js
@@ -4,12 +4,11 @@ import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
 export const PostList = (props) => (
   <List {...props}>
     <Datagrid rowClick="edit">
+      <TextField source="id" />
       <ReferenceField source="userId" reference="users">
         <TextField source="name" />
       </ReferenceField>
-      <TextField source="id" />
       <TextField source="title" />
-      <TextField source="body" />
     </Datagrid>
   </List>
 );
