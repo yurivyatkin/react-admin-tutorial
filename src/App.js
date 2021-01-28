06ece19b3ec47906d5030fe5d010a27dbcab4749
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-01-29 00:22:28 +1300

    Adding Login Page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ import UserIcon from '@material-ui/icons/Group';
 
 import jsonServerProvider from 'ra-data-json-server';
 
+import authProvider from './authProvider';
 import Dashboard from './Dashboard';
 import { UserList } from './users';
 import { PostList, PostEdit, PostCreate } from './posts';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin
+    authProvider={authProvider}
+    dashboard={Dashboard}
+    dataProvider={dataProvider}
+  >
     <Resource
       name="posts"
       list={PostList}
---
+++ src/authProvider.js
@@ -0,0 +1,33 @@
+// https://marmelab.com/react-admin/Tutorial.html#adding-a-login-page
+// The authProvider must expose 5 methods, each returning a Promise:
+const authProvider = {
+  // called when the user attempts to log in
+  login: ({ username }) => {
+    localStorage.setItem('username', username);
+    // accept all username/password combinations
+    return Promise.resolve();
+  },
+  // called when the user clicks on the logout button
+  logout: () => {
+    localStorage.removeItem('username');
+    return Promise.resolve();
+  },
+  // called when the API returns an error
+  checkError: ({ status }) => {
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('username');
+      return Promise.reject();
+    }
+    return Promise.resolve();
+  },
+  // called when the user navigates to a new location, to check for authentication
+  checkAuth: () => {
+    return localStorage.getItem('username')
+      ? Promise.resolve()
+      : Promise.reject();
+  },
+  // called when the user navigates to a new location, to check for permissions / roles
+  getPermissions: () => Promise.resolve(),
+};
+
+export default authProvider;
