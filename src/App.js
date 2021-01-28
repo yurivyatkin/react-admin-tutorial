500daf4601b93e4efc9586fcf98e0e7db522e90c
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2021-01-28 16:00:02 +1300

    Use custom PostList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import { UserList } from './users';
+import { PostList } from './posts';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
